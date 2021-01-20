b4c3115189a2df2ef77e9082e73cb81fff00f0b9
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-19 23:08:24 -0600

    commit tree f111dbb38bd31a2f0ef43c19beff88b93c964c29

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+
+function mapToNegativize(sourceArray){
+    return sourceArray.map(element => -1*element)
+}
+
+function mapToNoChange(sourceArray){
+    return [...sourceArray]
+} 
+function mapToDouble(sourceArray){
+    return sourceArray.map(element => 2*element)
+}
+function mapToSquare(sourceArray){
+    return sourceArray.map(element => element ** 2)
+} 
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    return sourceArray.reduce((accum, curr) => accum + curr, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray){
+    return sourceArray.filter(element => element == false).length == 0 
+} 
+
+function reduceToAnyTrue(sourceArray){
+    return sourceArray.filter(element => element == true).length > 0 
+}
+
+// console.log(reduceToAllTrue([1,false,2]))
